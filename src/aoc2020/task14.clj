4c2db8522403181370bfcc7f5399265de83c763d
Vasiliy Avdiushkin <dixelt@gmail.com>
2020-12-15 00:55:55 +0100

    task 14 2

--- src/aoc2020/task14.clj
+++ src/aoc2020/task14.clj
@@ -7,12 +7,23 @@
 
 (defn parse-mask [line]
   {:command :mask
-   :value (into [] (second (str/split line #"mask = ")))})
+   :binary-value (into [] (second (str/split line #"mask = ")))})
+
+(defn pad-bit-list [bit-list n]
+  (-> bit-list
+      (reverse)
+      (concat (repeat \0))
+      ((partial take n))
+      (reverse)))
 
 (defn parse-mem [line]
-  {:command :mem
-   :address (Long/parseUnsignedLong (second (re-find #"\[(.*)\]" line)))
-   :value (reverse (take 36 (concat (reverse (Long/toBinaryString (Long/parseUnsignedLong (second (re-find #"= (.*)" line))))) (repeat \0))))})
+  (let [address (Long/parseUnsignedLong (second (re-find #"\[(.*)\]" line)))
+        value (Long/parseUnsignedLong (second (re-find #"= (.*)" line)))]
+    {:command :mem
+     :address address
+     :binary-address (pad-bit-list (Long/toBinaryString address) 36)
+     :value value
+     :binary-value (pad-bit-list (Long/toBinaryString value) 36)}))
 
 (defn parse-line [line]
   (if (str/starts-with? line "mask")
@@ -23,20 +34,55 @@
   (map parse-line program))
 
 (defn truth-table [s-mask s-num]
-  (cond
-    (= s-mask \X) s-num
-    :else s-mask))
+  (case s-mask
+    \X s-num
+    s-mask))
 
-(defn apply-mask [mask num]
-  (Long/parseUnsignedLong (apply str (map truth-table mask num)) 2))
+(defn truth-table-addr [s-mask s-addr]
+  (case s-mask
+    \X \X
+    \1 \1
+    \0 s-addr))
 
-(defn part-1 [program]
+(defn get-address-set [addr]
+  (let [floating-addr (into [] addr)
+        bits (-> floating-addr
+                  frequencies
+                  (get \X))
+        combs (bit-shift-left 1 bits)
+        bstrings (->> (range combs)
+                      (map #(pad-bit-list (Long/toBinaryString %) bits))
+                      (map #(into [] %)))
+        x-indexes (->> floating-addr
+                       (map-indexed vector)
+                       (filter #(= (second %) \X))
+                       (map first))]
+    (->> bstrings
+         (map #(apply assoc floating-addr (interleave x-indexes %)))
+         (map #(apply str %))
+         (map #(Long/parseUnsignedLong % 2)))))
+
+(defn apply-mask [mask num truth-table]
+  (map truth-table mask num))
+
+(defn bit-list-to-num [bit-list]
+  (Long/parseUnsignedLong (apply str bit-list) 2))
+
+(defn mem-sum [program part]
   (loop [commands (parse-program program)
          mem {}
          mask (take 36 (repeat \X))]
     (if (empty? commands)
       (reduce + (vals mem))
-      (let [{:keys [command address value]} (first commands)]
+      (let [{:keys [command binary-address address binary-value value]} (first commands)]
         (case command
-          :mask (recur (rest commands) mem value)
-          :mem (recur (rest commands) (assoc mem address (apply-mask mask value)) mask))))))
+          :mask (recur (rest commands) mem binary-value)
+          :mem (recur (rest commands) (if (= part 1)
+                                        (assoc mem address (bit-list-to-num (apply-mask mask binary-value truth-table)))
+                                        (apply assoc mem (-> (apply-mask mask binary-address truth-table-addr)
+                                                           get-address-set
+                                                           (interleave (repeat value)))))
+                       mask))))))
+
+(println (mem-sum input 1))
+(println (mem-sum input 2))
