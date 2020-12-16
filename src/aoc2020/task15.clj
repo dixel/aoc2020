ffbf0ec209b3d40bc358a735e5339017c49f824a
Vasiliy Avdiushkin <dixelt@gmail.com>
2020-12-16 09:43:35 +0100

    task 15 - tiny refactor

--- src/aoc2020/task15.clj
+++ src/aoc2020/task15.clj
@@ -1,6 +1,7 @@
 (ns aoc2020.task15)
 
-(def input (->> [0,1,4,13,15,12,16]
+(def source [0,1,4,13,15,12,16])
+(def input (->> source
                 (map-indexed (fn [k v] [v (inc k)]))
                 (into {})))
 
@@ -10,9 +11,14 @@
       nil 0
       (- step previous-index))))
 
-(loop [board input
-       current 16
-       step (count input)]
-  (if (= 2020 step)
-    current
-    (recur (assoc board current step) (count-step board current step) (inc step))))
+(defn get-nth-round [game previous previous-index n]
+  (loop [board game
+         current previous
+         step previous-index]
+    (if (= n step)
+      current
+      (recur (assoc board current step) (count-step board current step) (inc step)))))
+
+(defn print-results []
+  (println "task 1: " (get-nth-round input (last source) (count source) 2020))
+  (println "task 2: " (get-nth-round input (last source) (count source) 30000000)))
